17908db6088da8caa5e1e17c0959e1063dd51abc
Cory Alford <calford4@my.westga.edu>
2016-01-30 20:31:59 -0800

    Added interfaces and public methods to both fragments

--- app/src/main/java/edu/westga/coryalfordstaticfragments/DataDisplayFragment.java
+++ app/src/main/java/edu/westga/coryalfordstaticfragments/DataDisplayFragment.java
@@ -1,11 +1,15 @@
 package edu.westga.coryalfordstaticfragments;
 
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 
 /**
@@ -13,17 +17,39 @@ import android.view.ViewGroup;
  */
 public class DataDisplayFragment extends Fragment {
 
+    TextView product;
+    double number1;
+    double number2;
+    double productDouble;
+
+    public void setNumber1(double number1) {
+        this.number1 = number1;
+    }
+
+    public void setNumber2(double number2) {
+        this.number2 = number2;
+    }
 
     public DataDisplayFragment() {
         // Required empty public constructor
     }
 
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_data_display, container, false);
+        View theView = inflater.inflate(R.layout.fragment_data_display, container, false);
+
+        this.product = (TextView) theView.findViewById(R.id.textView);
+
+        return theView;
     }
 
+    public void multiply() {
+        this.productDouble = this.number1 * this.number2;
+    }
+
+    public void displayProduct() {
+        this.product.setText(Double.toString(productDouble));
+    }
 }
--- app/src/main/java/edu/westga/coryalfordstaticfragments/DataEntryFragment.java
+++ app/src/main/java/edu/westga/coryalfordstaticfragments/DataEntryFragment.java
@@ -1,11 +1,14 @@
 package edu.westga.coryalfordstaticfragments;
 
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 
 
 /**
@@ -13,6 +16,14 @@ import android.view.ViewGroup;
  */
 public class DataEntryFragment extends Fragment {
 
+
+    EditText firstNumber;
+    EditText secondNumber;
+    DataEntryListener listener;
+
+    public interface DataEntryListener {
+        void onDataEntry(double numberOne, double numberTwo);
+    }
 
     public DataEntryFragment() {
         // Required empty public constructor
@@ -23,7 +34,30 @@ public class DataEntryFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_data_entry, container, false);
+        View theView = inflater.inflate(R.layout.fragment_data_entry, container, false);
+
+        this.firstNumber = (EditText) theView.findViewById(R.id.editText1);
+        this.secondNumber = (EditText) theView.findViewById(R.id.editText2);
+
+        Button multiplyButton = (Button) theView.findViewById(R.id.mult_button);
+        multiplyButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                multiplyButtonClicked(v);
+            }
+        });
+
+        return theView;
     }
 
+    private void multiplyButtonClicked(View v) {
+        double number1 = Double.parseDouble(this.firstNumber.getText().toString());
+        double number2 = Double.parseDouble(this.secondNumber.getText().toString());
+        listener.onDataEntry(number1, number2);
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        DataEntryListener listener = (DataEntryListener) context;
+    }
 }
